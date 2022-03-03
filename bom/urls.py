8da27421a0c14f7711402bf6a83c757e5a7a0824
Willis <williskneeland1234@gmail.com>
2022-03-02 23:21:52 -0700

    another try to fix workflow images not displaying

--- bom/urls.py
+++ bom/urls.py
@@ -9,7 +9,7 @@ from bom.third_party_apis import google_drive
 bom_patterns = [
     # BOM urls
     path('', views.home, name='home'),
-    # path('bom/', views.home, name='home'),
+    path('bom/', views.home, name='home'),
     path('create-organization/', views.organization_create, name='organization-create'),
     path('help/', views.Help.as_view(), name=views.Help.name),
     path('search-help/', views.search_help, name='search-help'),
